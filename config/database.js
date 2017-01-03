b999d9bbde5763f9c97144cbdb9732fa2db2ac2b
Juan D. Cardozo <jcardozo@ucsc.edu>
2017-01-03 13:00:15 -0800

    test heroku hosting

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.j
 module.exports = {
-
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+'url' :'mongodb://<dbuser>:<dbpassword>@ds155727.mlab.com:55727/heroku_sq5vpxq6'
+    //'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
