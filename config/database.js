e6ae27009bd0e0acee7791465d2075829ca63a7b
Juan D. Cardozo <jcardozo@ucsc.edu>
2017-01-03 13:17:10 -0800

    test2 mongolab

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.j
 module.exports = {
-'url' :'mongodb://<dbuser>:<dbpassword>@ds155727.mlab.com:55727/heroku_sq5vpxq6'
+'url':'mongodb://heroku_sq5vpxq6:f7ts0i03ekre35q93t74jqkudk@ds155727.mlab.com:55727/heroku_sq5vpxq6'
     //'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
